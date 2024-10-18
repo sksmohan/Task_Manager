a58ddbdf9416ccf811e9d886c07a54afaa50fdf8
sksmohan <sksmohanmis1@gmail.com>
2024-10-18 15:54:42 +0530

    mysql settings

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\task_manager\env
+set VIRTUAL_ENV=F:\task_manager_2\Task_manager\env
 
 if not defined PROMPT set PROMPT=$P$G
 
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'task_manager',
+        'USER': 'root',
+        'PASSWORD': 'sks_admin@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
